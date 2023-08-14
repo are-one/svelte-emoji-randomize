6faa400ead05d00a545d8e85e6628557add4a051
Are1PM <arwanpriantomangidi@gmail.com>
2023-08-14 09:55:00 +0800

    [UPDATE] add loading view

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,10 @@
 	import EmojiDescription from "./EmojiDescription.svelte";
 	import Button from "./Button.svelte";
 
+	let isLoaded = false;
 	let currentEmoji = '😁';
 	const emojis = ['🤣', '😊', '😍','🚀'];
+	let m = {x: 0, y: 0};
 
 	function randomizeEmoji() {
 		return emojis[Math.floor(Math.random() * emojis.length)];
@@ -13,6 +15,15 @@
 	function handleRandomButton() {
 		currentEmoji = randomizeEmoji();
 	}
+
+	function handleMousemove(event) {
+		m.x = event.clientX;
+		m.y = event.clientY;
+	}
+
+	setTimeout(function() {
+		isLoaded = true;
+	}, 2500);
 </script>
 
 <style>
@@ -25,9 +36,14 @@
 	<link rel="stylesheet" href="/terminal.min.css" />
 </svelte:head>
 
-<div class="container">
+<div class="container" on:mousemove={handleMousemove}>
+	<p>The mouse position: {m.x} x {m.y}</p>
 	<h1>Randomize Emoji</h1>
-	<EmojiDisplay {currentEmoji} />
-	<EmojiDescription />
-	<Button on:click={handleRandomButton} title={'🔁 Randomize'}/>
+	{#if isLoaded === true}
+		<EmojiDisplay {currentEmoji} />
+		<EmojiDescription />
+		<Button on:click={handleRandomButton} title={'🔁 Randomize'}/>
+	{:else}
+		<h2>Loading...</h2>
+	{/if}
 </div>
