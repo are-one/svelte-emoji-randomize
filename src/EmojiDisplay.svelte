166e0eae1a03fc2a2e2767d8701691b69e0601b8
Are1PM <arwanpriantomangidi@gmail.com>
2023-08-14 08:57:07 +0800

    [UPDATE] implement element class directive to button

--- src/EmojiDisplay.svelte
+++ src/EmojiDisplay.svelte
@@ -4,6 +4,7 @@
 		currentEmoji = '😥';
 	},1000);
 
+    let current = 'button1';
 </script>
 
 <style>
@@ -19,6 +20,19 @@
         font-size: 3em;
         margin-bottom: 0.5em;
     }
+
+    button {
+        color: orange;
+    }
+
+    .active {
+        background-color: #ff3e00;
+        color: #fff;
+    }
 </style>
 
 <div>{currentEmoji}</div>
+<button class:active={current == 'button1'} on:click={() => current = 'button1'}>Button 1</button>
+<button class:active={current == 'button2'} on:click={() => current = 'button2'}>Button 2</button>
+<button class:active={current == 'button3'} on:click={() => current = 'button3'}>Button 3</button>
+<p>{current.charAt(0).toUpperCase() + current.slice(1).toLowerCase()} di klik!</p>
