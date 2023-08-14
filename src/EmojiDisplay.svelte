a01ac100ba208612805ee313620671cb8fb2fa1f
Are1PM <arwanpriantomangidi@gmail.com>
2023-08-14 09:00:45 +0800

    [UPDATE] use class btn terminal.css to button

--- src/EmojiDisplay.svelte
+++ src/EmojiDisplay.svelte
@@ -21,18 +21,25 @@
         margin-bottom: 0.5em;
     }
 
-    button {
-        color: orange;
-    }
-
-    .active {
-        background-color: #ff3e00;
-        color: #fff;
-    }
 </style>
 
 <div>{currentEmoji}</div>
-<button class:active={current == 'button1'} on:click={() => current = 'button1'}>Button 1</button>
-<button class:active={current == 'button2'} on:click={() => current = 'button2'}>Button 2</button>
-<button class:active={current == 'button3'} on:click={() => current = 'button3'}>Button 3</button>
+<button
+    class="btn"
+    class:btn-primary={current == 'button1'}
+    on:click={() => current = 'button1'}
+    >Button 1</button>
+
+<button
+    class="btn"
+    class:btn-primary={current == 'button2'}
+    on:click={() => current = 'button2'}
+    >Button 2</button>
+    
+<button
+    class="btn"
+    class:btn-primary={current == 'button3'}
+    on:click={() => current = 'button3'}
+    >Button 3</button>
+
 <p>{current.charAt(0).toUpperCase() + current.slice(1).toLowerCase()} di klik!</p>
